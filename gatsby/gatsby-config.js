c1298439567d1f66ac23d7389f38cc7c69161f86
5adiyah <sadiali@pdx.edu>
2020-12-14 13:53:02 -0800

    Add sass plugin to config file

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,2 @@
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+[`gatsby-plugin-sass`];
