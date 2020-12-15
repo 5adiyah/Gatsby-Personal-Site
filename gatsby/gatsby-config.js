ac0776da68c1793d9b8c4cbe893fb5800e4ed7e2
5adiyah <sadiali@pdx.edu>
2020-12-14 17:42:21 -0800

    Get google fonts working

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -12,5 +12,28 @@ module.exports = {
         importer: globImporter(),
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Lato`,
+            variants: [`100`, `300`, `400`, `700`, `900`],
+          },
+          {
+            family: `Playfair Display`,
+            variants: [`400`, `500`, `600`, `700`, `800`, `900`],
+          },
+          {
+            family: `Heebo`,
+            variants: [`100`, `300`, `400`, `500`, `600`, `700`, `800`, `900`],
+          },
+          {
+            family: `Advent Pro`,
+            variants: [`400`, `500`, `600`, `700`],
+          },
+        ],
+      },
+    },
   ],
 };
